66597530acc1a2eb1b7ffbbfd02b35daad8877cc
Mylesryan100 <felicianomyles005@gmail.com>
2025-12-05 10:16:07 -0800

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 // Set up all variables in the .env file
 require('dotenv').config();
@@ -19,6 +20,7 @@ const app = express();
 // ========= Middlewares =================
 app.use(morgan('dev')); // logger
 app.use(express.json()); // body parser
+app.use(cors({origin: "http://localhost:5173"}))
 require('./config/passport');
 
 // ========= Routes ======================
